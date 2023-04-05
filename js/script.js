fd2f63eac4cca673cf36e24b35a9210f70f22867
sopyan <rifai.sopyan04@gmail.com>
2023-04-05 11:47:53 +0700

    Menambahkan animasi ketik

--- js/script.js
+++ js/script.js
@@ -10,3 +10,31 @@ menu.addEventListener("click", () => {
   muncul2.classList.toggle("muncul");
 });
 // Akhiran Navbar
+
+// Animasi Ketik
+const textElement = ["Web Developeer, Programmer & Designer"];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = "";
+let words = "";
+
+function ngetik() {
+  if (count == textElement.length) {
+    count = 0;
+  }
+
+  currentTxt = textElement[count];
+
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector(".ketik").textContent = words;
+
+  if (words.length == currentTxt.length) {
+    count++;
+    txtIndex = 0;
+  }
+
+  setTimeout(ngetik, 200);
+}
+
+ngetik();
+// Akhiran Animasi Ketik
